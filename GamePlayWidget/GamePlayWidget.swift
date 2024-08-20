47518c9c74ff4d1e49ed5d186bff128b35c5e96e
omar barkat <omarbarkat7@gmail.com>
2024-08-20 21:46:26 +0300

    adding Widget UI To Display Time and logo for now and data in widget come soon! :)

--- GamePlayWidget/GamePlayWidget.swift
+++ GamePlayWidget/GamePlayWidget.swift
@@ -20,7 +20,7 @@ struct Provider: AppIntentTimelineProvider {
     func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
         var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
@@ -41,16 +41,49 @@ struct GamePlayWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
+        ZStack {
+                   Image("defulatFootball")
+                       .resizable()
+                       .aspectRatio(contentMode: .fill)
+                       .edgesIgnoringSafeArea(.all)
+                   
+                   VStack(alignment: .leading, spacing: 8) {
+                       // App Name
+                       Text("Omar barkat")
+                           .font(.caption)
+                           .bold()
+                           .foregroundColor(.white)
+                           .padding(.top, 20)
 
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
-        }
-    }
+//                       // League Name
+//                       if let league = entry.league {
+//                           Text(league.name)
+//                               .font(.headline)
+//                               .foregroundColor(.white)
+//                       } else {
+//                           Text("Loading...")
+//                               .font(.headline)
+//                               .foregroundColor(.white)
+//                       }
+
+                       // Current Time
+                       Text("\(entry.date, formatter: timeFormatter)")
+                           .font(.caption)
+                           .foregroundColor(.white)
+                           .padding(.top, 110)
+
+                       Spacer()
+                   }
+                   .padding()
+               }
+           }
+    
 }
-
+private let timeFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "h:mm a"
+    return formatter
+}()
 struct GamePlayWidget: Widget {
     let kind: String = "GamePlayWidget"
 
